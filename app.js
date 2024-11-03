a68dac6aa2196c3dc63b5ea33a56cbf9974f1a9a
toyJig <rainbowtoyjig@gmail.com>
2024-11-03 00:38:35 -0400

    - added sockets and servers and backend and frontend and shi     - also has disconnect and connecting and general data syncing features     - yay i see ball you see ball

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const http = require('http')
+const server = http.createServer(app)
+
+const { Server } = require('socket.io')
+const io = new Server (server, {pingInterval: 2000, pingTimeout: 5000})
+
 const port = 3000
 
 app.use(express.static('public'))
@@ -8,6 +14,36 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+
+const players = {}
+
+
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  players[socket.id] = {
+    x: 500*Math.random(),
+    y: 500*Math.random(),
+    color: 'yellow'
+  }
+
+  socket.on('disconnect', (reason) =>{
+    console.log(reason)
+    delete players[socket.id]
+    io.emit('updatePlayers', players)
+  })
+
+
+  io.emit('updatePlayers', players)
+  console.log(players)
+})
+
+ 
+
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+console.log("Server loaded") 
